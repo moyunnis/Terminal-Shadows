53866c69c8d8eb837fb410337a7739b98e8fc517
модернизация <hvasyak@gmail.com>
2026-08-05 11:21:19 +0400

    Update updater.py

--- updater.py
+++ updater.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 class SimpleUpdater:
     def __init__(self):
-        self.repo_url = "https://github.com/moyunnis/Terminal-Shadows.git"
+        self.repo_url = "https://github.com/zoyuki/Terminal-Shadows.git"
         self.game_dir = os.path.dirname(os.path.abspath(__file__))
         self.data_dir = os.path.expanduser("~/.terminal_shadows")
         self.backup_dir = os.path.join(self.data_dir, "backups")
